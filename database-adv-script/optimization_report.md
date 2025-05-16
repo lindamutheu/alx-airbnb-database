da0b5d8b4a299e8757f76a40efdbe077c259b6c7
Alpha Infinos <kevinmullerhi@gmail.com>
2025-05-16 22:17:35 +0300

    optimization

---
+++ database-adv-script/optimization_report.md
@@ -0,0 +1,5 @@
+Write an initial query that retrieves all bookings along with the user details, property details, and payment details and save it on perfomance.sql
+
+Analyze the query’s performance using EXPLAIN and identify any inefficiencies.
+
+Refactor the query to reduce execution time, such as reducing unnecessary joins or using indexing.
---
+++ database-adv-script/performance.sql
@@ -0,0 +1,43 @@
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.status,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method,
+    pay.payment_date
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+
+
+
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.status,
+    b.total_price,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.amount,
+    pay.payment_method
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+
+
+
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
