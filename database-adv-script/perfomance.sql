3879378876f17b66fb73060cdf26a0b4adfc5dd0
Alpha Infinos <kevinmullerhi@gmail.com>
2025-05-18 20:46:50 +0300

    performance optimization

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,31 +1,29 @@
 ##Write an initial query that retrieves all bookings along with the user details, property details, and payment details and save it on perfomance.sql
 -- Initial query to retrieve bookings with user, property, and payment details
 
+Initial unoptimized query with WHERE and AND
+EXPLAIN
 SELECT
-  b.booking_id,
-  b.start_date,
-  b.end_date,
-  b.total_price,
-  b.status AS booking_status,
-  u.user_id,
-  u.first_name,
-  u.last_name,
-  u.email,
-  u.phone_number,
-  u.role,
-  p.property_id,
-  p.name AS property_name,
-  p.location AS property_location,
-  p.pricepernight,
-  pay.payment_id,
-  pay.amount AS payment_amount,
-  pay.payment_date,
-  pay.payment_method
+    b.booking_id,
+    b.status,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.name AS property_name,
+    p.location,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
-ORDER BY b.created_at DESC;
+WHERE b.status = 'confirmed'
+  AND b.start_date >= '2025-06-01';
+
 
 ##Refactor the query to reduce execution time, such as reducing unnecessary joins or using indexing.
 -- Step 2: Optimized query using existing indexes
